07a030b59308927c6a0f9ca3798f105a45dccb84
RominaMalimban <romina.lyn@hotmail.it>
2022-11-07 14:18:52 +0100

    dichiarazione variabile età utente

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ let chilometri= parseInt(prompt("Per favore, inserisci il numero di Km che vuoi 
 console.log(chilometri);
 
 // devo chiedere l'età del pax
-
-
+let eta = parseInt(prompt("E ora inserisci la tua età"));
+console.log(eta);
 
 
 // devo calcolare il costo del biglietto moltiplicando i km per 0.21 euro
