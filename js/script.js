01ede843507e5a2e4cdfb5a73862eafc08f45d21
RominaMalimban <romina.lyn@hotmail.it>
2022-11-07 14:13:22 +0100

    scomposizione della traccia

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Devo calcolare il prezzo del biglietto del treno, quindi:
+
+// devo chiedere il numero di Km da percorrere
+
+// devo chiedere l'età del pax
+
+// devo calcolare il costo del biglietto moltiplicando i km per 0.21 euro
+
+// condizioni:
+// - se eta minore di 18, allora avrà uno sconto del 20%
+// - se età maggiore di 65, allora avrà uno sconto del 40%
+
+
+// output: prezzo finale con massimo due numeri decimali. 
